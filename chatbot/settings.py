d9ad3d352e4a56dd51f7d8a095d10446a10df331
Tarun Upadhyay <tarunupadhyaydev2022@gmail.com>
2024-05-30 14:39:58 +0530

    updated requirement

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-hesw21(b@q*mm-h=@0+eac(=oxnxa#a+j8xm+4ta^=$ub4)e-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  False 
+DEBUG =  True 
 
 ALLOWED_HOSTS = ["*"]
 
@@ -139,4 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,7 @@ from answers.models import Answer
 class Post(models.Model):
     uid = models.CharField(max_length = 200, blank = True)
     message = models.CharField(max_length = 300, blank = True)
-    default_response = models.ManyToManyField(Qustion, null=True, blank=True, related_name="default_response")
+    default_response = models.ManyToManyField(Qustion, blank=True, related_name="default_response")
     bot_response = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True, blank=True, related_name="bot_response")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
